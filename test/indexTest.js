12f3ab80cc27b859966b69759873a7932ed7952b
beatriz <beatrizffeo@gmail.com>
2024-02-21 23:50:39 -0500

    introductions with parameter

--- test/indexTest.js
+++ test/indexTest.js
@@ -7,6 +7,9 @@ describe('introduction(name)', function() {
     expect(introduction("Samip")).toEqual("Hi, my name is Samip.");
   })
 })
+function introduction(name = "Aki") {
+  return `Hi, my name is ${name}.`;
+}
 
 describe('introductionWithLanguage(name, language)', function() {
   it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function() {
@@ -14,12 +17,18 @@ describe('introductionWithLanguage(name, language)', function() {
     expect(introductionWithLanguage("Samip", "React")).toEqual("Hi, my name is Samip and I am learning to program in React.");
   })
 })
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
 
 describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and language defaults to JavaScript', function() {
     expect(introductionWithLanguageOptional("Gracie")).toEqual("Hi, my name is Gracie and I am learning to program in JavaScript.");
   })
 })
+function introductionWithLanguageOptional (name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
 
 describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and the default value can be overridden with an argument', function() {
